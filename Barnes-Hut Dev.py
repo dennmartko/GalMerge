d7e73840e09008e67f0dbda0a6c6097f0c367879
Marten Lourens <martenlourens@gmail.com>
2020-09-08 21:47:46 +0200

    Created some data and plotting code to test our algorithm lateron.

--- Barnes-Hut Dev.py
+++ Barnes-Hut Dev.py
@@ -1,2 +1,15 @@
 import numpy as np
-import scipy as sc
+from matplotlib.pyplot import figure, show
+
+if __name__ == "__main__":
+    x, y = (20*np.random.random(size=10)-10, 20*np.random.random(size=10)-10)
+    
+    fig = figure(figsize=(10, 10))
+    frame = fig.add_subplot(1,1,1)
+    frame.scatter(x, y, color='k')
+    frame.grid()
+    frame.set_xlim(-10, 10)
+    frame.set_ylim(-10, 10)
+    frame.set_xlabel(r"$x$", fontsize=16)
+    frame.set_ylabel(r"$y$", fontsize=16)
+    show()
