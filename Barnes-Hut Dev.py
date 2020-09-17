1ea687dd4bb72224b5c8e166cdb291f5e5fb7033
Dennis <dennmartko@gmail.com>
2020-09-17 14:47:09 +0200

    removed mp

--- Barnes-Hut Dev.py
+++ Barnes-Hut Dev.py
@@ -152,37 +152,23 @@ def Tree(node, particles):
 			newmidR, newL = NewCellGeom(node.midR, node.L, 1)
 			D1 = Cell(newmidR, node.L / 2, parent=node, M = M1, R_CM = num1 / M1)
 			node.daughters.append(D1)
-			if len(obj) == 0:
-				p1 = Process(target=Tree, args=(D1,particles1,))
-				p1.start()
-			else:
-				Tree(D1, particles1)
+			Tree(D1, particles1)
 		if M2 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 2)
 			D2 = Cell(newmidR, newL, parent=node, M = M2, R_CM = num2 / M2)
 			node.daughters.append(D2)
-			if len(obj) == 0:
-				p2 = Process(target=Tree, args=(D2,particles2,))
-				p2.start()
-			else:
-				Tree(D2, particles2)
+			Tree(D2, particles2)
 		if M3 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 3)
 			D3 = Cell(newmidR, newL, parent=node, M = M3, R_CM = num3 / M3)
 			node.daughters.append(D3)
-			if len(obj) == 0:
-				p3 = Process(target=Tree, args=(D3,particles3,))
-				p3.start()
-			else:
-				Tree(D3, particles3)
+			Tree(D3, particles3)
 		if M4 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 4)
 			D4 = Cell(newmidR, newL, parent=node, M = M4, R_CM = num4 / M4)
 			node.daughters.append(D4)
 			Tree(D4, particles4)
 
-		if len(obj) == 1:
-			p1.join();p2.join();p3.join();
 
 def CellPlotter(cells, particles):
 	rectStyle = dict(fill=False, ec='lightgrey', lw=2, zorder=1)
