3084342705fe1f226506de4038130c110b37cc5d
Dennis <dennmartko@gmail.com>
2020-09-18 12:25:59 +0200

    GPU example added. mp removed.

--- Barnes-Hut Dev.py
+++ Barnes-Hut Dev.py
@@ -55,7 +55,7 @@ def rmParticles(rdd1, rdd2, rdd3, rdd4, particles1, particles2, particles3, part
 	return particles1, particles2, particles3, particles4 
 
 
-def FTree(node, particles, obj, q):
+def Tree(node, particles):
 	obj.append(node) # append the created node
 	# Hard copy the particle array
 	particles1 = particles.copy()
@@ -114,124 +114,28 @@ def FTree(node, particles, obj, q):
 	if pcount > 1:
 		#remove redundant particles from particles arrays
 		particles1, particles2, particles3, particles4 = rmParticles(np.array(rdd1), np.array(rdd2), np.array(rdd3), np.array(rdd4), particles1, particles2, particles3, particles4)
+
 		# if a potential cell's mass is nonzero create it!
 		if M1 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 1)
 			D1 = Cell(newmidR, node.L / 2, parent=node, M = M1, R_CM = num1 / M1)
 			node.daughters.append(D1)
-			p1 = Process(target=Startup, args=(D1,particles1,q))
-			p1.start()
-
+			Tree(D1, particles1)
 		if M2 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 2)
 			D2 = Cell(newmidR, newL, parent=node, M = M2, R_CM = num2 / M2)
 			node.daughters.append(D2)
-			p2 = Process(target=Startup, args=(D2,particles2,q))
-			p2.start()
+			Tree(D2, particles2)
 		if M3 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 3)
 			D3 = Cell(newmidR, newL, parent=node, M = M3, R_CM = num3 / M3)
 			node.daughters.append(D3)
-			p3 = Process(target=Startup, args=(D3,particles3,q))
-			p3.start()
-
+			Tree(D3, particles3)
 		if M4 != 0:
 			newmidR, newL = NewCellGeom(node.midR, node.L, 4)
 			D4 = Cell(newmidR, newL, parent=node, M = M4, R_CM = num4 / M4)
 			node.daughters.append(D4)
-			p4 = Process(target=Startup, args=(D4,particles4,q))
-			p4.start()
-			#Startup(D4,particles4,q)
-		p1.join();p2.join();p3.join();p4.join()
-			
-
-
-# Create a Tree = 1/4
-def Startup(node, particles, q):
-	obj = []
-	def Tree(node, particles):
-		obj.append(node) # append the created node
-		# Hard copy the particle array
-		particles1 = particles.copy()
-		particles2 = particles.copy()
-		particles3 = particles.copy()
-		particles4 = particles.copy()
-
-		# Redundant particles for each quadrant (the number in the variable name
-		# refers to the ith quadrant)
-		rdd1 = []; rdd1app = rdd1.append;
-		rdd2 = []; rdd2app = rdd2.append;
-		rdd3 = []; rdd3app = rdd3.append;
-		rdd4 = []; rdd4app = rdd4.append;
-
-		num1, num2, num3, num4, M1, M2, M3, M4 = Tree_template_init()
-
-		node.daughters = []
-
-		# Init
-		pcount = 0
-		# Check if more than 1 particles inside square
-		for indx, p in enumerate(particles):
-			r = p.r
-			m = p.m
-			condr = get_condr(r, node.L, node.midR) #condition r
-			if 1 > condr[0] > 0 and 1 > condr[1] > 0:
-				pcount += 1
-				rdd2app(indx)
-				rdd3app(indx)
-				rdd4app(indx)
-
-				num1, M1 = CM_Handler(num1,r,m,M1)
-			elif -1 < condr[0] < 0 and 1 > condr[1] > 0:
-				pcount += 1
-				rdd1app(indx)
-				rdd3app(indx)
-				rdd4app(indx)
-
-				num2, M2 = CM_Handler(num2,r,m,M2)
-			elif -1 < condr[0] < 0 and -1 < condr[1] < 0:
-				pcount += 1
-				rdd1app(indx)
-				rdd2app(indx)
-				rdd4app(indx)
-
-				num3, M3 = CM_Handler(num3,r,m,M3)
-			elif 1 > condr[0] > 0 and -1 < condr[1] < 0:
-				pcount += 1
-				rdd1app(indx)
-				rdd2app(indx)
-				rdd3app(indx)
-
-				num4, M4 = CM_Handler(num4,r,m,M4)
-
-		# If theres more than one particle in a node, we can create new nodes!
-		if pcount > 1:
-			#remove redundant particles from particles arrays
-			particles1, particles2, particles3, particles4 = rmParticles(np.array(rdd1), np.array(rdd2), np.array(rdd3), np.array(rdd4), particles1, particles2, particles3, particles4)
-
-			# if a potential cell's mass is nonzero create it!
-			if M1 != 0:
-				newmidR, newL = NewCellGeom(node.midR, node.L, 1)
-				D1 = Cell(newmidR, node.L / 2, parent=node, M = M1, R_CM = num1 / M1)
-				node.daughters.append(D1)
-				Tree(D1, particles1)
-			if M2 != 0:
-				newmidR, newL = NewCellGeom(node.midR, node.L, 2)
-				D2 = Cell(newmidR, newL, parent=node, M = M2, R_CM = num2 / M2)
-				node.daughters.append(D2)
-				Tree(D2, particles2)
-			if M3 != 0:
-				newmidR, newL = NewCellGeom(node.midR, node.L, 3)
-				D3 = Cell(newmidR, newL, parent=node, M = M3, R_CM = num3 / M3)
-				node.daughters.append(D3)
-				Tree(D3, particles3)
-			if M4 != 0:
-				newmidR, newL = NewCellGeom(node.midR, node.L, 4)
-				D4 = Cell(newmidR, newL, parent=node, M = M4, R_CM = num4 / M4)
-				node.daughters.append(D4)
-				Tree(D4, particles4)
-	Tree(node,particles)
-	q.put(obj)
+			Tree(D4, particles4)
 
 
 def CellPlotter(cells, particles):
@@ -270,7 +174,6 @@ if __name__ == "__main__":
 
 	L = 20
 	obj = []
-	q = Manager().Queue()
 	# compute the location of the Center of Mass (COM) and total mass for the
 	# ROOT cell
 	Rgal_CM = np.sum([p.m * p.r for p in particles]) / np.sum([p.m for p in particles])
@@ -280,11 +183,9 @@ if __name__ == "__main__":
 	ROOT = Cell(np.array([0, 0]), L, parent=None, M=Mgal, R_CM=Rgal_CM)
 
 	start = time.time()
-	FTree(ROOT, particles, obj, q)
+	Tree(ROOT, particles)
 	print("Tree built!")
 	end = time.time()
 	duration = end - start
-
-	obj = obj + q.get() + q.get() + q.get() + q.get()
 	print("\nTOTAL AMOUNT OF CELLS: ",len(obj))
 	print("TOTAL TIME TAKEN FOR",len(particles), " PARTICLES IS: ",duration, "SECONDS!")
